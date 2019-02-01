f4b10e509346e12c295ecee245002429f34afeec
Derrick Smith <derricksmith01@msn.com>
2019-01-31 20:18:40 -0700

    Update hook.php

--- hook.php
+++ hook.php
@@ -48,18 +48,18 @@ function plugin_phpsaml_install() {
 	global $DB;
 
 	if (!$DB->tableExists("glpi_plugin_phpsaml_configs")) {
-      $query = "CREATE TABLE `glpi_plugin_phpsaml_configs` (
-				  `id` int(11) NOT NULL auto_increment,
-				  `strict` int(2) NOT NULL,
-				  `debug` int(2) NOT NULL,
-                  `saml_sp_certificate` text collate utf8_unicode_ci NOT NULL,
-				  `saml_sp_certificate_key` text collate utf8_unicode_ci NOT NULL,
-				  `saml_idp_entity_id` varchar(128) collate utf8_unicode_ci NOT NULL,
-				  `saml_idp_single_sign_on_service` varchar(128) collate utf8_unicode_ci NOT NULL,
-				  `saml_idp_single_logout_service` varchar(128) collate utf8_unicode_ci NOT NULL,
-				  `saml_idp_certificate` text collate utf8_unicode_ci NOT NULL,
-                  PRIMARY KEY  (`id`)
-               ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci";
+      		$query = "CREATE TABLE `glpi_plugin_phpsaml_configs` (
+			`id` int(11) NOT NULL auto_increment,
+			`strict` int(2) NOT NULL,
+			`debug` int(2) NOT NULL,
+			`saml_sp_certificate` text collate utf8_unicode_ci NOT NULL,
+			`saml_sp_certificate_key` text collate utf8_unicode_ci NOT NULL,
+			`saml_idp_entity_id` varchar(128) collate utf8_unicode_ci NOT NULL,
+			`saml_idp_single_sign_on_service` varchar(128) collate utf8_unicode_ci NOT NULL,
+			`saml_idp_single_logout_service` varchar(128) collate utf8_unicode_ci NOT NULL,
+			`saml_idp_certificate` text collate utf8_unicode_ci NOT NULL,
+                  	PRIMARY KEY  (`id`)
+               ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci";
       $DB->query($query) or die("error creating glpi_plugin_phpsaml_configs ". $DB->error());
 	  
 	  $query = "INSERT INTO `glpi_plugin_phpsaml_configs`
