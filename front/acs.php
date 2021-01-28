0b404e13fb8e4cde3a99869ff27e2d8ec4cdb53c
Derrick Smith <derricksmith01@msn.com>
2021-01-28 09:40:22 -0700

    Update ACS to use phpsaml settings

--- front/acs.php
+++ front/acs.php
@@ -1,8 +1,8 @@
 <?php
-
 if (!defined('GLPI_ROOT')) {
     define('GLPI_ROOT', '../../..');
 }
+
 
 
 $post = $_POST;
@@ -10,26 +10,57 @@ unset($_POST);
 
 include (GLPI_ROOT.'/inc/includes.php');
 
+require_once GLPI_ROOT.'/plugins/phpsaml/lib/xmlseclibs/xmlseclibs.php';
+$libDir = GLPI_ROOT.'/plugins/phpsaml/lib/php-saml/src/Saml2/';
+		
+$folderInfo = scandir($libDir);
+
+foreach ($folderInfo as $element) {
+	if (is_file($libDir.$element) && (substr($element, -4) === '.php')) {
+		require_once $libDir.$element;
+	}
+}
+
+use OneLogin\Saml2\Settings;
+use OneLogin\Saml2\Response;
+
+$error = null;
+$phpsaml = new PluginPhpsamlPhpsaml();
+$settings = $phpsaml::$phpsamlsettings;
+		
 try {
     if (isset($post['SAMLResponse'])) {
-        $samlSettings = new OneLogin\Saml2\Settings(PluginPhpsamlPhpsaml::$phpsamlsettings);
+        $samlSettings = new OneLogin\Saml2\Settings($settings);
         $samlResponse = new OneLogin\Saml2\Response($samlSettings, $post['SAMLResponse']);
         if ($samlResponse->isValid()) {
-			PluginPhpsamlPhpsaml::$nameid = $samlResponse->getNameId();
-			PluginPhpsamlPhpsaml::$userdata = $samlResponse->getAttributes();
-			PluginPhpsamlPhpsaml::$nameidformat = PluginPhpsamlPhpsaml::$auth->getNameIdFormat();
-			PluginPhpsamlPhpsaml::$sessionindex = PluginPhpsamlPhpsaml::$auth->getSessionIndex();
+			$phpsaml::$nameid = $samlResponse->getNameId();
+			$phpsaml::$userdata = $samlResponse->getAttributes();
+			$phpsaml::$nameidformat = $phpsaml::$auth->getNameIdFormat();
+			$phpsaml::$sessionindex = $phpsaml::$auth->getSessionIndex();
 			try {
-				PluginPhpsamlPhpsaml::glpiLogin((isset($post['RelayState']) && $post['RelayState'] != '' ? $post['RelayState'] : ''));
+				$phpsaml::glpiLogin((isset($post['RelayState']) && $post['RelayState'] != '' ? $post['RelayState'] : ''));
 			} catch(Exception $e) {
-				Toolbox::logInFile("php-errors", $e->getMessage() . "\n", true);
+				$error = $e->getMessage();
+				Toolbox::logInFile("php-errors", $error . "\n", true);
 			}
         } else {
-			Toolbox::logInFile("php-errors", 'Invalid SAML Response' . "\n", true);
+			$error = "Invalid SAML Response";
+			Toolbox::logInFile("php-errors", $error . "\n", true);
         }
     } else {
-		Toolbox::logInFile("php-errors", 'No SAML Response found in POST.' . "\n", true);
+		$error = "No SAML Response found in POST.";
+		Toolbox::logInFile("php-errors", $error . "\n", true);
     }
 } catch (Exception $e) {
-	Toolbox::logInFile("php-errors", 'Invalid SAML Response: ' . $e->getMessage() . "\n", true);
+	$error = $e->getMessage();
+	Toolbox::logInFile("php-errors", $error . "\n", true);
+}
+
+if($error){
+	Html::nullHeader("Login", $CFG_GLPI["root_doc"] . '/index.php');
+	echo '<div class="center b">'.$error.'<br><br>';
+	// Logout whit noAUto to manage auto_login with errors
+	echo '<a href="' . $CFG_GLPI["root_doc"] .'/index.php">' .__('Log in again') . '</a></div>';
+	Html::nullFooter();
+	exit();
 }
