26f67ad5f56d79808fa10ab84c04a2b5be18817c
Derrick Smith <derricksmith01@msn.com>
2021-06-25 08:55:22 -0600

    Add nameid format database column

--- install/update.class.php
+++ install/update.class.php
@@ -42,9 +42,18 @@ class PluginPhpsamlUpdate {
 			Toolbox::logInFile("php-errors", "Upgrading to 1.0.0" . "\n", true);
 			self::do_109();
 		}
-		if(self::$installed_version == '1.0.9'){
+		if(self::$installed_version < '1.1.0'){
 			Toolbox::logInFile("php-errors", "Upgrading to 1.1.0"  . "\n", true);
 			self::do_110();
+		}
+		if(self::$installed_version < '1.1.1'){
+			Toolbox::logInFile("php-errors", "Upgrading to 1.1.1"  . "\n", true);
+			self::do_111();
+		}
+		
+		if(self::$installed_version < '1.1.2'){
+			Toolbox::logInFile("php-errors", "Upgrading to 1.1.2"  . "\n", true);
+			self::do_112();
 		}
 	}
 	
@@ -78,6 +87,21 @@ class PluginPhpsamlUpdate {
 		if ($DB->error()) Toolbox::logInFile("php-errors", $DB->error()  . "\n", true);
 		
 		self::set_installed_version("1.1.0");
+	}
+	
+	public static function do_111(){
+		global $DB;
+		self::set_installed_version("1.1.1");
+	}
+	
+	public static function do_112(){
+		global $DB;
+			
+		$query = "ALTER TABLE `glpi_plugin_phpsaml_configs` ADD saml_sp_nameid_format varchar(255) after saml_sp_certificate_key";
+		$DB->query($query);
+		if ($DB->error()) Toolbox::logInFile("php-errors", $DB->error()  . "\n", true);
+		
+		self::set_installed_version("1.1.2");
 	}
 }
 ?>
